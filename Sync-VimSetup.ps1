d37b0e24eb2f0829ba683fb891f4f5272b9ac1c8
Korimer <thekclov@gmail.com>
2025-09-30 17:42:20 -0600

    baseline

---
+++ Sync-VimSetup.ps1
@@ -0,0 +1,29 @@
+winget install Neovim.Neovim
+
+$nvide = 'neovide.exe'
+$nvimbin = 'C:\Program Files\Neovim\bin'
+$nvimcfg = "$env:USERPROFILE\AppData\Local\nvim"
+
+Invoke-Webrequest 'https://github.com/neovide/neovide/releases/latest/download/neovide.exe.zip' -OutFile "$nvide.zip"
+7z x "$nvide.zip"
+rm "$nvide.zip"
+
+$nvimlink = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Neovim\Neovim.lnk'
+$redirect = ".\Neovim.lnk"
+
+Start-Process powershell -Verb RunAs -ArgumentList @(
+    '-c'
+    @(
+        "cd $(Get-Location)"
+        "mv '$nvide' '$nvimbin' -Force"
+        "mv '$redirect' '$nvimlink' -Force"
+    ) -join ';'
+)
+
+New-Item $nvimcfg -ItemType Directory -Force
+
+@(
+    "Sync-VimSetup.ps1"
+    ".git"
+
+) | mv $nvimcfg
