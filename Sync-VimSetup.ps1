01240deea35c435dea233c111e57efaa616f9b95
Korimer <thekclov@gmail.com>
2025-10-01 12:35:05 -0600

    shoot bring back the powershell installer lol

---
+++ Sync-VimSetup.ps1
@@ -0,0 +1,30 @@
+winget install Neovim.Neovim
+
+$nvide = 'neovide.exe'
+$nvimbin = 'C:\Program Files\Neovim\bin'
+$nvimcfg = "$env:USERPROFILE\AppData\Local\nvim"
+
+Invoke-Webrequest 'https://github.com/neovide/neovide/releases/latest/download/neovide.exe.zip' -OutFile "$nvide.zip"
+7z x "$nvide.zip"
+rm "$nvide.zip"
+
+$nvimlink = 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Neovim\Neovim.lnk'
+$redirect = ".\Neovim.lnk"
+
+Start-Process powershell -Verb RunAs -ArgumentList @(
+    '-c'
+    @(
+        "cd $(Get-Location)"
+        "mv '$nvide' '$nvimbin' -Force"
+        "mv lua $nvimbin"
+        "mv '$redirect' '$nvimlink' -Force"
+    ) -join ';'
+)
+
+New-Item $nvimcfg -ItemType Directory -Force
+
+@(
+    "init.lua"
+    "Sync-VimSetup.ps1"
+    ".git"
+) | % {mv $_ $nvimcfg}
