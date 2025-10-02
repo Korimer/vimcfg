f8b69718082b7953d0212b05a850cac17e36e392
Korimer <thekclov@gmail.com>
2025-10-01 18:53:42 -0600

    more progress, almost done with my first customization

---
+++ lua/tweaks/init.lua
@@ -0,0 +1,4 @@
+cnoremap <expr> <up> wildmenumode() ? "\<left>" : "\<up>"
+cnoremap <expr> <down> wildmenumode() ? "\<right>" : "\<down>"
+cnoremap <expr> <left> wildmenumode() ? "\<up>" : "\<left>"
+cnoremap <expr> <right> wildmenumode() ? " \<bs>\<C-Z>" : "\<right>"
