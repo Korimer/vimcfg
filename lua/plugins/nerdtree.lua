2553c32f92aedd70be7ecd1d2c4429c04202de5b
Korimer <thekclov@gmail.com>
2025-10-15 17:05:38 -0600

    nice tree

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -1,11 +1,19 @@
 return {
-    "preservim/nerdtree"
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    lazy = false, -- neo-tree will lazily load itself
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    opt = {
+        close_if_last_window = true
+    },
     config = function()
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = "nerdtree"
-            callback = function()
-                vim.keymap.set('n','<C-CR>','<cmd>lua vim.print("hi hi it worked")<CR>', { buffer = true })
-            end
-        })
-    end
+        vim.api.nvim_create_autocmd("VimLeavePre", {command = ":Neotree close",})
+        vim.api.nvim_create_autocmd("VimEnter", {command = "Neotree"})
+    end,
+  }
 }
