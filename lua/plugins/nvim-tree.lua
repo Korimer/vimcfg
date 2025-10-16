b82895841e18eed17e521f382a7c985b4c9ab8eb
Korimer <thekclov@gmail.com>
2025-10-16 15:22:08 -0600

    ok well nvim tree just works

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+return {
+--  {
+--    "nvim-tree/nvim-web-devicons",
+--    lazy = false
+--  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    opts = {},
+    lazy = false
+  }
+}
