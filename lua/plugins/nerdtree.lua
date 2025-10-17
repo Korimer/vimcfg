f3edd63a44f28f2f752ed7026dde6466b310a85f
Korimer <thekclov@gmail.com>
2025-10-17 16:01:59 -0600

    Epic sessioning win

--- lua/plugins/nerdtree.lua
+++ lua/plugins/nerdtree.lua
@@ -9,14 +9,28 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     opts = {
-        auto_clean_after_session_restore = true 
+        auto_clean_after_session_restore = true,
+        close_if_last_window = true,
+        window = {
+            mappings = {
+                ["P"] = {
+                    "toggle_preview",
+                    config = {
+                        use_float = false,
+                        -- use_image_nvim = true,
+                        -- use_snacks_image = true,
+                        -- title = 'Neo-tree Preview',
+                    },
+                },
+            }
+        }
     },
-    config = function()
-        vim.api.nvim_create_autocmd("VimLeavePre", {
-            callback = function() 
-                vim.schedule(function() vim.cmd(":Neotree close") end)
-            end
-        })
+    init = function()
+--        vim.api.nvim_create_autocmd("VimLeavePre", {
+--            callback = function() 
+--                vim.schedule(function() vim.cmd(":Neotree close") end)
+--            end
+--        })
         vim.api.nvim_create_autocmd("VimEnter", {
             callback = function() 
                 vim.schedule(function() vim.cmd("Neotree focus") end)
