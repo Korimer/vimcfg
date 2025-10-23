6be7dc03985065fcf3cb55487742ebf507df5dca
Korimer <thekclov@gmail.com>
2025-10-23 16:43:33 -0600

    sync

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,17 +1,48 @@
---local capabilities = require('cmp_nvim_lsp').default_capabilities()
---for _, v in ipairs(require('plugins.completion.to-install')['lsplist']) do
---  vim.lsp.enable(v)
---  require('config.lsp.global')
---  vim.print(vim.lsp.config[v])--[capabilities] = capabilities
---  vim.print(vim.lsp.config[v])
---end
+local target_langs = {
+  c = {"clangd"},
+  cpp = {"clangd"}, 
+  diff = {},
+  lua = {"lua_ls"},
+  luadoc = {},
+  markdown = {},
+  markdown_inline = {},
+  printf = {},
+  regex = {},
+  vim = {},
+  vimdoc = {},
+  java = {"jdtls"}, --,"java-test","java-debug-adapter"},
+  rust = {"rust_analyzer"}
+}
+
+local i = 1
+local parserlist = {}
+local lspset = {}
+for k, v in pairs(target_langs) do
+  parserlist[i] = k
+  i = i+1
+  for _, dep in ipairs(v) do
+    lspset[dep] = true
+  end
+end
+
+local j = 1
+local lsplist = {}
+for k, v in pairs(lspset) do
+    lsplist[j] = k
+    j = j+1
+end
 
 local lsppath = vim.fn.stdpath("config") .. "/lua/config/lsp"
 
-for _, file in ipairs(vim.fn.readdir(lsppath)) do
-  local lsp_setup = vim.print(require('config.lsp.' .. file:gsub("%.lua$","")))
+for _, file in ipairs(lspset) do
+  local lsp_setup = require('config.lsp.' .. file:gsub("%.lua$",""))
   local lsp_name = lsp_setup[1]
   local lsp_cfg = lsp_setup[2]
   vim.lsp.config[lsp_name] = lsp_cfg
   vim.lsp.enable(lsp_name)
 end
+
+return {
+  ["parserlist"] = parserlist,
+  ["lsplist"] = lsplist
+}
