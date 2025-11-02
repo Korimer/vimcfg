21efbba94cd998b36a53c75893d3737872d7a51f
Korimer <thekclov@gmail.com>
2025-11-02 00:50:00 -0600

    oops forgot the end statement

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.g.sysname = vim.loop.os_uname().sysname
 local sys_upper = string.upper(vim.g.sysname)
 local systems = {"WINDOWS","LINUX","MINGW","DARWIN"}
 for i=1, 4 do
-  if string.match(sys_upper,systems[i]) then vim.g.env = systems[i]
+  if string.match(sys_upper,systems[i]) then vim.g.env = systems[i] end
 end
 
 -- give me my extensions (lol)
