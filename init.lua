b3d8b9737ed7f9c944ea2b012d288d805e95caa9
Korimer <thekclov@gmail.com>
2025-11-02 00:47:18 -0600

    working towards multi-system compatibility

--- init.lua
+++ init.lua
@@ -1,5 +1,14 @@
 -- give me a leader button
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- give me knowledge of what os i have
+vim.g.sysname = vim.loop.os_uname().sysname
+local sys_upper = string.upper(vim.g.sysname)
+local systems = {"WINDOWS","LINUX","MINGW","DARWIN"}
+for i=1, 4 do
+  if string.match(sys_upper,systems[i]) then vim.g.env = systems[i]
+end
+
 -- give me my extensions (lol)
 require('config.lazy')
