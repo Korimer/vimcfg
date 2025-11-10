75c719ce7c5cd65ac349df510ae355000a199ef3
Korimer <thekclov@gmail.com>
2025-11-09 23:36:34 -0700

    setting up multicursor

--- plugin/remappings.lua
+++ plugin/remappings.lua
@@ -1,2 +1,7 @@
-
-
+vim.api.nvim_create_user_command("KillShada",function ()
+  local shada_root = vim.fn.stdpath('data') .."/shada"
+  local shadafiles = vim.fn.readdir(shada_root)
+  for i=1, #shadafiles do
+    os.remove(shada_root .. '/' .. shadafiles[i] )
+  end
+end, {})
