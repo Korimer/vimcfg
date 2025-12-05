d9c47f520439186e071de60ad498aee87f7fb5b1
Korimer <thekclov@gmail.com>
2025-12-04 17:02:24 -0700

    fixed autosession neotree incompatibility

--- lua/extensions/unstructured/file-navigation/auto-session.lua
+++ lua/extensions/unstructured/file-navigation/auto-session.lua
@@ -1,9 +1,6 @@
 return {
   "rmagatti/auto-session",
   lazy = false,
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { "~/", "~/vimsessions", "~/Downloads", "/", ""},
     auto_restore_last_session = true,
@@ -13,6 +10,16 @@ return {
     legacy_cmds = false,
     cwd_change_handling = false,
     pre_save_cmds = { 'Neotree close' },
-    post_restore_cmds = { 'Neotree filesystem show' },
+    post_restore_cmds = { function()
+        local buffset = vim.api.nvim_list_bufs()
+        for i=1, #buffset do
+          local buf_name = vim.api.nvim_buf_get_name(buffset[i])
+          if string.find(buf_name,"neo-tree fillesystem [1]") then
+            vim.api.nvim_buf_delete(buffset[i],{force=true})
+          end
+        end
+        vim.cmd("Neotree filesystem show")
+      end
+    },
   },
 }
