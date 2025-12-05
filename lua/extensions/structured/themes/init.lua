5f67dff999a88f369f249975c3b0274d2ce8162c
Korimer <thekclov@gmail.com>
2025-12-04 18:08:57 -0700

    trying to make cyclical colorschemes

--- lua/extensions/structured/themes/init.lua
+++ lua/extensions/structured/themes/init.lua
@@ -1,4 +1,25 @@
-return { 
+vim.g.boot_colorscheme = "zaibatsu"
+
+local extensions = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "folke/tokyonight.nvim", prioity = 1000, lazy = false, opts = {} },
+  { "EdenEast/nightfox.nvim", priority = 1000, lazy = false, opts = {} }
 }
+
+vim.keymap.set("n","<A-=>", function()
+    if vim.g.all_colorschemes[vim.g.colorscheme_selected+1] == nil
+      then vim.g.colorscheme_selected = 1
+      else vim.g.colorscheme_selected = vim.g.colorscheme_selected + 1
+    end
+    vim.cmd("colorscheme " .. vim.g.all_colorschemes[vim.g.colorscheme_selected])
+  end
+)
+
+vim.api.nvim_create_autocmd("User", {
+  pattern="VeryLazy",
+  callback=function ()
+    local all_colorschemes = vim.fn.getcompletion('',"color")
+    vim.g.colorscheme_selected = (function() for i,v in ipairs(all_colorschemes) do if v==vim.g.boot_colorscheme then return i end end end)()
+  end
+})
+return extensions
--- plugin/preferences.lua
+++ plugin/preferences.lua
@@ -1,5 +1,5 @@
 vim.o.wildmenu = true
 vim.o.wildoptions = "pum"
-vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme " .. vim.g.boot_colorscheme)
 vim.diagnostic.config({ virtual_text = false, virtual_lines = { current_line = true} })
 vim.o.number = true
