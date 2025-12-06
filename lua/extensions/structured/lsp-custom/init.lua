303dc177a7958865a6d8014a18a2c23e100d62db
Korimer <thekclov@gmail.com>
2025-12-06 16:19:29 -0700

    playing with indentation + fixed lsp imports

--- lua/extensions/structured/lsp-custom/init.lua
+++ lua/extensions/structured/lsp-custom/init.lua
@@ -1,7 +1,9 @@
-local full_spec = {require('extensions.structured.lsp-custom.lspconfig')}
-local external_lsp_repos = vim.api.nvim_get_runtime_file("lua/extensions/lsp-custom/ext/*.lua",true)
+local external_lsp_repos = vim.api.nvim_get_runtime_file("lua/extensions/structured/lsp-custom/ext/*.lua",true)
+local full_spec = { require('extensions.structured.lsp-custom.lspconfig') }
 for i=1, #external_lsp_repos do
   full_spec[i+1] = require('extensions.structured.lsp-custom.ext.' .. vim.fn.fnamemodify(external_lsp_repos[i], ':t:r'))
 end
+
+vim.print(full_spec)
 
 return full_spec
--- lua/extensions/unstructured/completion/treesitter.lua
+++ lua/extensions/unstructured/completion/treesitter.lua
@@ -1,17 +1,27 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  
+
   event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
   cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
   opts_extend = { "ensure_installed" },
 
   opts = {
     ensure_installed = require('config.lang')['parserlist'], -- filled in by language-servers.lua
-    indent = { enable = true }, ---@type lazyvim.TSFeat
-    folds = { enable = true }, ---@type lazyvim.TSFeat
+    indent = { enable = true },
+    folds = { enable = true },
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false
-    }, ---@type lazyvim.TSFeat
-  }
+    },
+  },
+  init = function()
+    vim.g.python_indent = {
+      closed_paren_align_last_line = true,
+      continue = "shiftwidth() * 2",
+      disable_parentheses_indenting = true,
+      nested_paren = "shiftwidth()",
+      open_paren = "shiftwidth() * 2",
+      searchpair_timeout = 150
+    }
+  end
 }
