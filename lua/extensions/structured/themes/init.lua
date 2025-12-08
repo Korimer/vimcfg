5161a2136d5adb507c733be17f5a3c9afbbb3217
Korimer <thekclov@gmail.com>
2025-12-08 13:05:00 -0700

    added more themes + cycling

--- lua/extensions/structured/themes/init.lua
+++ lua/extensions/structured/themes/init.lua
@@ -1,9 +1,13 @@
-vim.g.boot_colorscheme = "zaibatsu"
+vim.g.boot_colorscheme = "unokai"
 
 local extensions = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "folke/tokyonight.nvim", prioity = 1000, lazy = false, opts = {} },
-  { "EdenEast/nightfox.nvim", priority = 1000, lazy = false, opts = {} }
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "folke/tokyonight.nvim", opts = {} },
+  { "EdenEast/nightfox.nvim", opts = {} },
+  { "AlexvZyl/nordic.nvim", opts = {} },
+  { "bluz71/vim-moonfly-colors", opts = {} },
+  { "Mofiqul/vscode.nvim", opts = {} },
+  { "craftzdog/solarized-osaka.nvim", opts = {} },
 }
 
 vim.keymap.set("n","<A-=>", function()
@@ -14,12 +18,21 @@ vim.keymap.set("n","<A-=>", function()
     vim.cmd("colorscheme " .. vim.g.all_colorschemes[vim.g.colorscheme_selected])
   end
 )
+vim.keymap.set("n","<A-->", function()
+    if vim.g.all_colorschemes[vim.g.colorscheme_selected-1] == nil
+      then vim.g.colorscheme_selected = #vim.g.all_colorschemes
+      else vim.g.colorscheme_selected = vim.g.colorscheme_selected -1
+    end
+    vim.cmd("colorscheme " .. vim.g.all_colorschemes[vim.g.colorscheme_selected])
+  end
+)
+
 
 vim.api.nvim_create_autocmd("User", {
-  pattern="VeryLazy",
+  pattern="LazyDone",
   callback=function ()
-    local all_colorschemes = vim.fn.getcompletion('',"color")
-    vim.g.colorscheme_selected = (function() for i,v in ipairs(all_colorschemes) do if v==vim.g.boot_colorscheme then return i end end end)()
+    vim.g.all_colorschemes = vim.fn.getcompletion('',"color")
+    vim.g.colorscheme_selected = (function() for i,v in ipairs(vim.g.all_colorschemes) do if v==vim.g.boot_colorscheme then return i end end end)()
   end
 })
 return extensions
