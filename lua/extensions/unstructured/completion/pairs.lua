dc3997d4bb4eff2a5f275711547760a9b467aff6
Korimer <thekclov@gmail.com>
2025-12-14 23:52:12 -0700

    almost decent treesitter based pairing. meh

--- lua/extensions/unstructured/completion/pairs.lua
+++ lua/extensions/unstructured/completion/pairs.lua
@@ -1,5 +1,36 @@
+local function has_closing_delimiter_after_cursor(bufnr, cursor_col)
+  local ts_utils = require('nvim-treesitter.ts_utils')
+  local node = ts_utils.get_node_at_cursor()
+  if not node then
+    return false
+  end
+  while node do
+    local type = node:type()
+
+    if
+      type == "parenthesized_expression"
+      or type == "argument_list"
+      or type == "parameters"
+      or type == "tuple"
+      or type == "list"
+      or type == "array"
+    then
+      local _, _, _, end_col = node:range()
+
+      if end_col > cursor_col then
+        return true
+      end
+    end
+
+    node = node:parent()
+  end
+
+  return false
+end
+
 return {
   {
+    -- Note that we screw up literally every keybinding if the setup function for this gets run
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     config = function ()
@@ -14,18 +45,19 @@ return {
   {
     'steelsojka/pears.nvim',
     dependencies = {"windwp/nvim-autopairs"},
-    opts = function(conf)
-      vim.print(conf)
-      conf.expand_on_enter(false)
+    config = function() 
+      require("pears").setup(function (conf)
+        conf.on_enter(
+          function () return require'nvim-autopairs'.completion_confirm()
+        end)
+        conf.pair("(", {
+          close = ")",
+          should_expand = function(args)
+            local row, col = unpack(args.cursor)
+            return not has_closing_delimiter_after_cursor(args.bufnr, col)
+          end
+        })
+      end)
     end,
-    config = function(_,opts)
-      require("pears").setup(opts)
-      vim.api.nvim_set_keymap(
-        'i',
-        '<CR>',
-        "v:lua.require'nvim-autopairs'.completion_confirm()",
-        { expr = true, noremap = true, desc = "autopairs completion confirm" }
-      )
-    end
   }
 }
