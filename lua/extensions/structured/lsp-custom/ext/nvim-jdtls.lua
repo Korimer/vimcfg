b33126f280b4e45822b052579de305df455b675e
Korimer <thekclov@gmail.com>
2026-02-01 00:06:19 -0700

    ok testing jdtls normally but we might drop the python wrapper

--- lua/extensions/structured/lsp-custom/ext/nvim-jdtls.lua
+++ lua/extensions/structured/lsp-custom/ext/nvim-jdtls.lua
@@ -1,3 +1,22 @@
+local sep = vim.g.path_sep
+local mason_dir = vim.fn.stdpath('data') .. sep .. "mason" .. sep .. "packages" .. sep .."bin"
+
 return {
-  "mfussenegger/nvim-jdtls"
+  "mfussenegger/nvim-jdtls",
+
+  config = function ()
+    vim.lsp.config('jdtls', {
+      cmd = function (dispatchers, client_config)
+         vim.lsp.rpc.start(
+           {'jdtls', "--java-executable", mason_dir .. "java.cmd"},
+           dispatchers,
+           {
+             cwd = client_config.cmd_cwd,
+             env = client_config.cmd_env,
+             detached = client_config.detached
+           }
+         )
+      end
+    })
+  end
 }
