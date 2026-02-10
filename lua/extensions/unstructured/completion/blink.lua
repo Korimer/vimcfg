2ce1920737e1ffa31b32597564cfd0865ec53900
Korimer <thekclov@gmail.com>
2026-02-10 13:43:44 -0700

    dream completion config

--- lua/extensions/unstructured/completion/blink.lua
+++ lua/extensions/unstructured/completion/blink.lua
@@ -1,30 +1,45 @@
 -- as opposed to .182
-
 return {
-  'saghen/blink.cmp',
-  -- add luasnip..?
-  dependencies = {  },
+  "saghen/blink.cmp",
+  dependencies = { "L3MON4D3/LuaSnip" },
   -- apparently release tag = download prebuilt?
   version = '1.*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     keymap = {
-      preset = 'none',
-    
-      ['<Tab>'] = {
-        function(cmp)
-          if has_words_before() then
-            return cmp.insert_next()
-          end
-        end,
-        'fallback',
-      },
-      ['<S-Tab>'] = { 'insert_prev' },
+      preset = 'super-tab',
+      --['<Tab>'] = {
+      --  function(cmp)
+      --    vim.print("blink proc lol")
+      --    if false then
+      --      return cmp.insert_next()
+      --    end
+      --  end,
+      --  'fallback',
+      --},
+      --['<S-Tab>'] = { 'insert_prev' },
     },
+
     completion = {
-      menu = { enabled = false },
-      list = { selection = { preselect = false }, cycle = { from_top = false } },
+      documentation = { auto_show = false },
+      ghost_text = {
+        enabled = true,
+        show_with_menu = true
+      },
+      menu = {
+        enabled = true,
+        auto_show = false
+      },
+      --list = { selection = { preselect = false }, cycle = { from_top = false } },
     },
-  }
+
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' }
+    },
+
+    snippets = { preset = 'luasnip' },
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" }
 }
--- lua/extensions/unstructured/completion/luasnip.lua
+++ lua/extensions/unstructured/completion/luasnip.lua
@@ -1,5 +1,40 @@
+local friendly_extends = {
+  typescript = { 'tsdoc' },
+  javascript = { 'jsdoc' },
+  lua = { 'luadoc' },
+  python = { 'pydoc' },
+  rust = { 'rustdoc' },
+  java = { 'javadoc' },
+  c = { 'cdoc' },
+  cpp = { 'cppdoc' },
+  php = { 'phpdoc' },
+  sh = { 'shelldoc' },
+}
+
 return {
     "L3MON4D3/LuaSnip",
     -- follow latest release.
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    dependencies = {
+      "rafamadriz/friendly-snippets"
+    },
+    -- lmao imagine using js
+    --run = "make install_jsregexp"
+    opts = { history = true, delete_check_events = 'TextChanged' },
+
+    config = function(_,opts)
+      local snip = require('luasnip')
+      snip.setup(opts)
+      require('luasnip.loaders.from_vscode').lazy_load()
+      for ft, snips in pairs(friendly_extends) do
+        require('luasnip').filetype_extend(ft, snips)
+      end
+
+      vim.keymap.set({"i","n"},"<C-Tab>",function ()
+        snip.jump(1)
+      end,{noremap=true, silent=true})
+      vim.keymap.set({"i","n"},"<C-S-Tab>",function ()
+        snip.jump(-1)
+      end,{noremap=true, silent=true})
+    end,
 }
