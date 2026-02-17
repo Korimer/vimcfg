3845503be85bb2c7fb9213790cf0dfd337568a83
Korimer <thekclov@gmail.com>
2026-02-16 19:30:16 -0700

    polishing

--- plugin/preferences.lua
+++ plugin/preferences.lua
@@ -1,11 +1,17 @@
-vim.o.wildmenu = true
-vim.o.wildoptions = "pum"
+vim.opt.wildmenu = true
+vim.opt.wildoptions = "pum"
 vim.cmd("colorscheme " .. vim.g.boot_colorscheme)
 vim.diagnostic.config({ virtual_text = false, virtual_lines = { current_line = true} })
-vim.o.number = true
 vim.keymap.set("i","<C-r>+", "<C-r><C-o>+", {noremap=true, silent=true})
 vim.keymap.set({"n","v"}," ","<NOP>", {noremap=true, silent=true})
 vim.g.neovide_scale_factor = 0.8
+
+vim.opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+-- give me tabs like a normal editor PLEASE
+vim.opt.expandtab = true;
+vim.opt.softtabstop = 2;
+vim.opt.shiftwidth = 2;
+vim.opt.smarttab = true;
 
 -- two. hundred. opts
 vim.opt.cursorline = true
--- plugin/vim-defaults.lua
+++
@@ -1,7 +0,0 @@
--- give me tabs like a normal editor PLEASE
-vim.opt.expandtab = true;
-vim.opt.softtabstop = 2;
-vim.opt.shiftwidth = 2;
-vim.opt.smarttab = true;
--- give me session options?
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
