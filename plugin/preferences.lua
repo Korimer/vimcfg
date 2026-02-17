f24ae979314ef1cb2ff1cc6964d36d84eaf68f97
Korimer <thekclov@gmail.com>
2026-02-16 19:15:11 -0700

    trying to get more consistent opts

--- plugin/preferences.lua
+++ plugin/preferences.lua
@@ -6,6 +6,13 @@ vim.o.number = true
 vim.keymap.set("i","<C-r>+", "<C-r><C-o>+", {noremap=true, silent=true})
 vim.keymap.set({"n","v"}," ","<NOP>", {noremap=true, silent=true})
 vim.g.neovide_scale_factor = 0.8
-vim.o.cursorline = true
+
+-- two. hundred. opts
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.autoindent = false
+vim.opt.smartindent = true
+vim.opt.cindent = false
+-- and two more
 vim.api.nvim_create_autocmd("InsertEnter", { command = [[set norelativenumber]] })
 vim.api.nvim_create_autocmd("InsertLeave", { command = [[set relativenumber]] })
