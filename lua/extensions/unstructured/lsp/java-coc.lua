c1859cc1a825f852b361872a9ce82b90e37f7881
Korimer <thekclov@gmail.com>
2026-03-21 00:22:52 -0600

    sync

--- lua/extensions/unstructured/lsp/java-coc.lua
+++ lua/extensions/unstructured/lsp/java-coc.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  pattern = "*.jj",
+  callback = function()
+    vim.bo.filetype = "java"
+  end,
+})
+
 return {
   'nvim-java/nvim-java',
   enabled = vim.g.distro_name ~= "NixOS",
