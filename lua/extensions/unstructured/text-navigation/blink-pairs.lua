afd5c6ed7d2fa54e1769972f25ac9a8b1a89bf69
Korimer <thekclov@gmail.com>
2026-03-30 18:21:56 -0600

    trying out blink pairs

---
+++ lua/extensions/unstructured/text-navigation/blink-pairs.lua
@@ -0,0 +1,26 @@
+return {
+  priority = 1000,
+  'saghen/blink.pairs',
+  dependencies = { 'saghen/blink.download' },
+  opts = {
+    mappings = {
+      enabled = true,
+      cmdline = true,
+      pairs = {},
+    },
+    highlights = {
+      enabled = true,
+      cmdline = true,
+      groups = { 'BlinkPairsOrange', 'BlinkPairsPurple', 'BlinkPairsBlue' },
+      unmatched_group = 'BlinkPairsUnmatched',
+      matchparen = {
+        enabled = true,
+        cmdline = false, -- highlight is currently bugged
+        include_surrounding = true,
+        group = 'BlinkPairsMatchParen',
+        priority = 250
+      }
+    },
+    debug = false
+  }
+}
