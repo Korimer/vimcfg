120524769ffd615c3cf8512f4a4fc08f6dad3609
= <=>
2026-06-21 23:22:40 -0600

    just like home; neotree now automaticall runs on restore

--- lua/extensions/unstructured/file-navigation/ori-sessions.lua
+++ lua/extensions/unstructured/file-navigation/ori-sessions.lua
@@ -3,5 +3,6 @@ return {
   dir = "/home/korimer/Documents/cs/ori-sessions/"
   ;opts = {
     restore_prev_session = true
+    ;workspace_enable_hook = function() vim.cmd("Neotree filesystem show") end
   }
 }
